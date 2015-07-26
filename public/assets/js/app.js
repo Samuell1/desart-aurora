a38c4abff4ef05914eaa2cc2258183f8bb71431a
Samuel <samuell.patro@gmail.com>
2015-07-26 20:52:38 +0200

    View index

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,2 +1,61 @@
-$(function(){
+$(function () {
+
+$('.usernotifications .feed').perfectScrollbar();
+$('.ui.sidebar').sidebar('attach events', '.togglesidebar');
+
+$('.show-userprofile')
+  .popup({
+    on: 'click',
+    popup : $('.userprofile'),
+    hoverable: true,
+    position : 'bottom right',
+    offset: -14,
+    delay: {
+      show: 300,
+      hide: 500
+    }
+});
+
+$('.show-usernotifications')
+  .popup({
+    on: 'click',
+    popup : $('.usernotifications'),
+    hoverable: true,
+    position : 'bottom right',
+    offset: -14,
+    delay: {
+      show: 300,
+      hide: 500
+    },
+    onVisible: function() {
+      $('.show-usernotifications .label').remove();
+    }
+});
+
+var content = [
+  { title: 'Veenex je pán' },
+  { title: 'banan' },
+];
+
+$('.ui.search')
+.search({
+  source: content
+});
+
+if($(".show-usernotifications .label").length > 0)
+{
+
+  var timer = setInterval(function() {
+
+    $('.show-usernotifications .label')
+      .transition({
+        animation : 'pulse',
+        duration  : 300,
+      })
+    ;
+
+  }, 2000);
+}
+
+
 });
