c0b8b55230c8e91552ee9f10e49e672b07c2fab1
KotasMan <kotas.koterba@hotmail.sk>
2015-07-27 22:22:53 +0200

    podstatna vec

--- App/Library/Auth.php
+++ App/Library/Auth.php
@@ -2,9 +2,21 @@
 
 namespace App\Library;
 
+use Aurora\Session;
+
 class Auth
 {
 
-	# ...
+	protected $Session;
 
+	public function __construct(Session $Session)
+	{
+		$this->Session = $Session;
+	}
+
+
+	public function createUser($username, $password, $mail = "foo@bar.com", $activated = false)
+	{
+
+	}
 }
