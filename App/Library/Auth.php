1660d4d86afcc5e88a869bfd07fa47c6ae9b85a6
KotasMan <kotas.koterba@hotmail.sk>
2015-07-27 22:23:35 +0200

    Revert "podstatna vec"
    This reverts commit fdfe4047c4b6e59864943f040642aabea96d1052.

--- App/Library/Auth.php
+++ App/Library/Auth.php
@@ -2,21 +2,9 @@
 
 namespace App\Library;
 
-use Aurora\Session;
-
 class Auth
 {
 
-	protected $Session;
+	# ...
 
-	public function __construct(Session $Session)
-	{
-		$this->Session = $Session;
-	}
-
-
-	public function createUser($username, $password, $mail = "foo@bar.com", $activated = false)
-	{
-
-	}
 }
