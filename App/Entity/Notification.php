d3336a03cd777c971043a6ab53ffdddb00a3ce18
Samuell <samuell.patro@gmail.com>
2015-07-27 16:40:00 +0200

    Notification model

---
+++ App/Entity/Notification.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Entity;
+
+use Spot\Mapper;
+use Spot\Entity;
+
+class Notification extends Entity
+{
+   protected static $table = 'notification';
+
+   public static function fields()
+   {
+      return [
+         'id'    => ['type' => 'smallint', 'primary' => true, 'autoincrement' => true],
+      ];
+   }
+
+   public static function relations(\Spot\MapperInterface $mapper, \Spot\EntityInterface $entity)
+   {
+      return [
+          'Meta' => $mapper->hasOne($entity, 'App\Entity\User', 'user_id')
+      ];
+   }
+
+}
