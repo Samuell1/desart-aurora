d585780211dc85af73e809286125aed6da80f164
KotasMan <kotas.koterba@hotmail.sk>
2015-07-28 15:04:19 +0200

    Created Auth controller

---
+++ App/Controller/Auth.php
@@ -0,0 +1,37 @@
+<?php 
+
+namespace App\Controller;
+
+class Auth
+{
+
+	public function __construct()
+	{
+		
+		# ...
+
+	}
+
+
+	public function register()
+	{
+
+		# ...
+
+	}
+
+	public function login()
+	{
+
+		# ...
+
+	}
+
+	public function logout()
+	{
+
+		# ...
+
+	}
+
+}
