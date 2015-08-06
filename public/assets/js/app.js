1f46a80a092685f132769a6e5890c303e8f01a55
Samuell <samuell.patro@gmail.com>
2015-08-06 18:12:43 +0200

    push ma ass

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -6,7 +6,8 @@
 
 $(function () {
 
-$('.popup').popup();
+$('.popup-active').popup();
+
 $('.usernotifications .feed').perfectScrollbar();
 $('.ui.sidebar').sidebar('attach events', '.togglesidebar');
 
