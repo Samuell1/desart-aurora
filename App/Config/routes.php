30ec85eac97bbb718499f0ced3a8850cb667faa0
VeeeneX <veeenex@gmail.com>
2015-08-18 19:46:07 +0200

    Fix kvoli mergu

--- App/Config/routes.php
+++ App/Config/routes.php
@@ -13,14 +13,14 @@ $Router->get("/clanok/{slug}", ["Article", "view"], ["name" => "article"]);
 $Router->get("/clanky", ["Article", "overview"]);
 
 // forum
-$Router->get("/tema/{slug}", ["Forum\\Topic", "view"], "topic");
+$Router->get("/tema/{slug}", ["Forum\\Topic", "view"], ["name" => "topic"]);
 $Router->get("/tema/?{slug}/strana/?{page}", ["Forum\\Topic", "view"]);
 $Router->get("/forum/kategoria/?{slug}/strana/?{page}", ["Forum\\Topic", "overviewCategory"]);
 $Router->get("/forum/kategoria/?{category}", ["Forum\\Topic", "overviewCategory"]);
 $Router->get("/forum", ["Forum\\Category", "overview"]);
 
 // user
-$Router->get("/account/logout",  ["App\\Controller\\Auth", "logout"], ["name" => "logout"]);
+$Router->get("/account/logout",  ["Auth", "logout"], ["name" => "logout", "namespace" => "App\\Controller\\"]);
 $Router->get("/account/settings",  ["User", "settings"], ["name" => "settings"]);
 $Router->get("/user/{uid}", ["User", "view"], [
     "name" => "profile"
@@ -29,6 +29,21 @@ $Router->get("/{uid}", ["User", "view"], [
     "name" => "profile"
 ])->where(["uid" => "(@[A-z0-9]++)"]);
 
+$Router->mount("/ajax", function($Router) {
+    $Router->post("/login", ["Auth", "login"]);
+    $Router->get("/search", ["Search", "search"]);
+    $Router->post("/register", ["Auth", "register"]);
+
+    $Router->get("/subscribe/{type}/{id}", ["Subscribe", "subscribe"]);
+    $Router->get("/unsubscribe/{type}/{id}", ["Subscribe", "unsubscribe"]);
+
+    $Router->post("/topic/add/{num}", ["Forum\\Topic", "add"]);
+    $Router->post("/post/add/{num}", ["Forum\\Post", "add"]);
+
+    $Router->post("/comment/add", ["Comment", "add"]);
+}, [
+    "namespace" => "App\\Controller\\"
+]);
 try {
    $found = $Router->findRoute($Router->findRequestMethod(), $Router->findUri());
 } catch (RouteNotFoundException $Exception) {
